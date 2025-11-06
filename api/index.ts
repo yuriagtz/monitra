4459495ce67d2812cfa0847f8b1ee662f4f1634b
yuriagtz <g.tz.kaz@gmail.com>
2025-11-06 18:13:36 +0900

    Silence TS7016 for build output import

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "http";
+// @ts-expect-error dist/index.js はビルド生成物のため型定義が存在しません
 import handler from "../dist/index.js";
 
 export default handler as (
