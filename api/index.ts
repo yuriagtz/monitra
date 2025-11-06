675999427986451fa1aa1ac7ee8b8c667eca0a11
yuriagtz <g.tz.kaz@gmail.com>
2025-11-06 18:00:41 +0900

    Add type shim for Vercel handler export

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,7 @@
+import type { IncomingMessage, ServerResponse } from "http";
+// dist/index.js は build 後に生成されるサーバレスハンドラー
 export { default } from "../dist/index.js";
+
+declare const _default: (req: IncomingMessage, res: ServerResponse) => unknown;
+export default _default;
 
